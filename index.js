ffa86a44a9f21cc955fd21b184ee498cc2a26d19
Jamie Yoon <rkawk3316@gmail.com>
2020-05-03 03:29:56 +0300

    Added graphql

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const graphql = require("express-graphql");
+
+const app = express();
+app.use("/graphql", (req, res) => {
+  graphql({
+    //schema: graphqlschema,
+    graphiql: true,
+    context: { req, res },
+  });
+});
+app.listen(process.env.HTTP_PORT);
