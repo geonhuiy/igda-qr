f7b283f4710acee1a63affb90d4fabdab310bfe1
Jamie Yoon <rkawk3316@gmail.com>
2020-05-06 15:44:10 +0300

    Changed index.js

--- index.js
+++ index.js
@@ -40,14 +40,5 @@ app.use("/graphql", (req, res) => {
 });
 
 db.on("connected", () => {
-  process.env.NODE_ENV = process.env.NODE_ENV || "development";
-  if (process.env.NODE_ENV === "production") {
-    require("./production")(app, process.env.PORT);
-  } else {
-    require("./localhost")(
-      app,
-      process.env.HTTP_PORT,
-      process.env.HTTPS_PORT
-    );
-  }
+  app.listen(process.env.HTTP_PORT);
 });
