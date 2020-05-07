f5ed3e90f45d197fecb6299a1221b43456a55c3a
Jamie Yoon <rkawk3316@gmail.com>
2020-05-07 06:30:38 +0300

    Fixed member info stacking issue

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -22,7 +22,7 @@ const deleteEvent = async(req,res) => {
   })
 }
 const attendeeCheckin = async(req, res) => {
-  let query = `mutation{attendeeCheckIn(attendeeId:"${req.params.attendeeId}",eventId:"${req.params.eventId}"){name,attendees{firstname,lastname}}}`;
+  let query = `mutation{attendeeCheckIn(attendeeId:"${req.body.attendeeId}",eventId:"${req.body.eventId}"){name,attendees{firstname,lastname}}}`;
   graphql(schema, query).then((result) => {
     res.json(result);
   })
